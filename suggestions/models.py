6638a6688104d2a84c35b44d5808aca3eab89e03
wasifmohammad6@gmail.com <Administrator@Tanveer-47735.wgbd.com>
2023-07-23 17:38:58 +0600

    version_3

--- suggestions/models.py
+++ suggestions/models.py
@@ -3,29 +3,32 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
 from django_userforeignkey.models.fields import UserForeignKey
+from autoslug import AutoSlugField
 
 
 class Product(models.Model):
     product_name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     suggestions = models.ManyToManyField('self', blank=True, symmetrical=False)
-    slug = models.SlugField(unique=True, null= True, blank=True)
     description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='products/', null=True, blank= True)
-    #created_date = models.DateTimeField(auto_now_add=True, default=timezone.now)
-    #created_by = UserForeignKey(auto_user_add=True, on_delete=models.SET_NULL, related_name="created_products")
-    #updated_date = models.DateTimeField(auto_now=True)
-    #updated_by = UserForeignKey(auto_user=True, on_delete=models.SET_NULL, related_name="updated_products")
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
 
     def __str__(self):
         return self.product_name
 
+
+
+
+
+
+"""
     def get_suggestions(self, product):
         # Retrieve the related suggestions and serialize them
         suggestions = ProductSerializer(product.suggestions.all(), many=True).data
         return suggestions
 
-"""
+
+
 from django.db import models
 
 
--- suggestions/serializers.py
+++ suggestions/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-    suggestions = serializers.SerializerMethodField()
-    #suggestions = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    #suggestions = serializers.SerializerMethodField()
+    suggestions = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Product
         fields = '__all__'
